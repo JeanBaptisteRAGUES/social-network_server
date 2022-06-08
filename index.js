ffc6aa0ae53027174992ef4aebc0cf5ff72a8e6c
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-06-08 04:47:14 +0200

    ...

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-//test
-
 const PORT = process.env.PORT || 5000;
 
 (async function(){
