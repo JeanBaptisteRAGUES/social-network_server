a74697a3cfa6bb0752b1531c0938e1aebdcf7028
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-06-09 17:04:40 +0200

    Change gitignore

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://jbragues31:Megalodon31@cluster0.xppzw.mongodb.net/social-network-with-MERNG_db?retryWrites=true&w=majority',
+    SECRET_KEY: 'gs5f54erg6sk5u4lu64c1q50'
+}
